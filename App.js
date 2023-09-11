9a7db7178956545acbf78e5f42f855bc740a5950
Gauravcodeit <adhikarigaurav82@gmail.com>
2023-09-11 23:20:16 +0530

    Used Jsx Elment and added a script in package json to run build easily

--- App.js
+++ App.js
@@ -1,40 +1,17 @@
 import  React  from "react";
 import ReactDOM  from "react-dom/client";
-/*
-<div class="parent">
-    <div id="child1">
-        <h1>this h1 tag</h1>
-        <h2>this h2 tag</h2>
-    </div>
-    <div id="child12">
-        <h1>this h1 tag</h1>
-        <h2>this h2 tag</h2>
-    </div>
-</div>
-*/
 
-const reactElement = React.createElement("div", { class: "parent"},
-        [
-            React.createElement("div", { id: "child1"},
-            [
-                React.createElement("h1",{},"this h1 tag" ),
-                React.createElement("h2",{},"this h2 tag" )
-            ]),
-            React.createElement("div", { id: "child2"},
-            [
-                React.createElement("h1",{},"this h1 tag" ),
-                React.createElement("h2",{},"this h2 tag" )
-            ])
-
-        ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(reactElement)
 
-// /* React DOM Manipulation */
-// const reactElement = React.createElement("h1", {id:"heading", class:"new-heading"}, "Hello world from react")
-// console.log(reactElement);
+// React Element
+const reactElement = React.createElement("h1", { className: "parent"}, "This is an react elment")
 
-// const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(reactRoot);
-// reactRoot.render(reactElement)
-// /* React DOM Manipulation */
+console.log(reactElement);
+// root.render(reactElement)
+// React Element
+
+// JSX Element
+const jsxelment = <h1 className='parent'>this is an jsx element</h1>;
+console.log(jsxelment);
+root.render(jsxelment);
+// JSX Element
