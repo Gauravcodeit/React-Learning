bb0dea0bdea647c8b10883a16784c42eb02b6804
Gauravcodeit <adhikarigaurav82@gmail.com>
2023-11-29 19:30:54 +0530

    added a filter in swiggy clone

--- src/component/CardContainer.js
+++ src/component/CardContainer.js
@@ -1,11 +1,34 @@
 import {restaurant} from '../../Data';
 import Card from './Card';
+import { useState } from 'react';
 const CardContainer = function (){
+    const [listRestaurant, setListRestaurant] = useState(restaurant);
+    const [clickedFlag, setclickedFlag] =useState(true)
 
+
+    function topRated(){
+        console.log(listRestaurant.length)
+        clickedFlag ? setclickedFlag(false): setclickedFlag(true);
+        let FilteredRestaurant = listRestaurant.filter((res)=>{
+            return (res.info.avgRating > 4.1)
+        })
+        setListRestaurant(FilteredRestaurant)
+        console.log(FilteredRestaurant)
+    }
+    function allRestaurant(){
+        setListRestaurant(restaurant)
+        clickedFlag ? setclickedFlag(false): setclickedFlag(true);
+    }
     return (
-        <div className="card-grp">
+        <>
+        <div className='filter-sort '>
+           <button className='top-rated-btn' type='button' onClick={ clickedFlag ? topRated : allRestaurant}>
+                { clickedFlag ? "Top Rated Restaurant" :"All Restaurant"}
+            </button>
+        </div>
+         <div className="card-grp">
             {
-                restaurant.map((item, index)=>{
+                listRestaurant.map((item, index)=>{
                     return(
                         <Card
                         image_url={ item?.info?.cloudinaryImageId }
@@ -17,6 +40,8 @@ const CardContainer = function (){
                 })
             }
         </div>
+        </>
+
     )
 }
 export default CardContainer
