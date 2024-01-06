ded365658b7ad1d7f044eedc03911805cd110a62
Gauravcodeit <adhikarigaurav82@gmail.com>
2024-01-06 13:21:02 +0530

    added cors proxy

--- src/component/CardContainer.js
+++ src/component/CardContainer.js
@@ -12,9 +12,11 @@ const CardContainer = function (){
         fetchData()
     }, [])
     async function fetchData(){
-        const restaurants = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.630251&lng=77.3463139&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING') ;
+        //const restaurants = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.630251&lng=77.3463139&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING') ;
+        // cors policy bypass - https://corsproxy.org/
+        const restaurants = await fetch('https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D28.630251%26lng%3D77.3463139%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING') ;
         const restaurantJson = await restaurants.json();
-        const topRestaurant = restaurantJson?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        const topRestaurant = restaurantJson?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
         console.log(restaurantJson)
         setListRestaurant(topRestaurant);
         setfilteredRestaurant(topRestaurant);
@@ -40,13 +42,13 @@ const CardContainer = function (){
         })
         setfilteredRestaurant(filteredList)
     }
-    if (!listRestaurant?.length){
-        return(
-            <>
-            <Shimmer />
-            </>
-        )
-    }
+    // if (!listRestaurant?.length){
+    //     return(
+    //         <>
+    //         <Shimmer />
+    //         </>
+    //     )
+    // }
 
     return (
         <>
