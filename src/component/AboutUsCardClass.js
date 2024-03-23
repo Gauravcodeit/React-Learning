783054d1bddcd172ce7539cf41cbd3f38facf3db
Gauravcodeit <adhikarigaurav82@gmail.com>
2024-03-23 12:42:36 +0530

    class component

--- src/component/AboutUsCardClass.js
+++ src/component/AboutUsCardClass.js
@@ -1,10 +1,31 @@
 import React from "react";
 class AboutUsCardClass extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            age: 0,
+            expenseString: "0"
+        };
+        console.log(this.props?.newprop + " Component Constructor")
+    }
+    componentDidMount(){
+        console.log(this.props?.newprop + "Component DID Mount")
+    }
     render(){
+        console.log(this.props?.newprop + "Component Render Function")
         return (
             <>
                 <div>
-                    details from class
+                    details from class {this.props?.newprop}
+
+                    { " " + this.state.age }
+                    <button onClick={ ()=>{
+                            this.setState({
+                                age : this.state.age + 1,
+                            })
+                    }}>
+                    Counter
+                     </button>
                 </div>
             </>
         )
