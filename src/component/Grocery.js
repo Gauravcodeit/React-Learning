834fdbc0dafa68a5bc59163317bf5453d8538ee6
Gauravcodeit <adhikarigaurav82@gmail.com>
2024-03-25 19:21:10 +0530

    worked on changes in lazy loaded

---
+++ src/component/Grocery.js
@@ -0,0 +1,11 @@
+const Grocery =()=>{
+    return (
+        <div className="container">
+            <div>
+                Grocery List Compoenent
+            </div>
+        </div>
+
+    )
+}
+export default Grocery;
--- src/component/Header.js
+++ src/component/Header.js
@@ -16,6 +16,9 @@ const  Header = ()=>{
                     <li>
                         <Link to={'/about'}>About</Link>
                     </li>
+                    <li>
+                        <Link to={'/grocery'}>Grocery</Link>
+                    </li>
                 </ul>
             </div>
         </div>
